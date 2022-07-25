304290e182f003778a25ef25fc41a6cc7e72b07d
antonysa23-meet <antony.saleh2017@gmail.com>
2022-07-25 16:03:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+function bark(){
+	let mySound = new Audio('my_audio_file.wav')
+	mySound.play()
+}
 function changeBackgroundColor(){
 	input = document.getElementById("color").value
 	document.body.style.backgroundColor = input;
+	bark()
 }
